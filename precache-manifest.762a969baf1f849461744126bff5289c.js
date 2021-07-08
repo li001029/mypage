f00b724ee30beccfb9d5a088e8ad9892b6c6bb6e
Jiafeng Li <li001029@umn.edu>
2021-07-08 12:23:50 -0500

    Updates

---
+++ precache-manifest.762a969baf1f849461744126bff5289c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772a0061ecb46139df2dfa20b34bdd01",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "b9b485e37bbb681be774",
+    "url": "/mypage/static/css/2.2c431122.chunk.css"
+  },
+  {
+    "revision": "7377b98dc63450c2ca7f",
+    "url": "/mypage/static/css/main.57eeedf2.chunk.css"
+  },
+  {
+    "revision": "b9b485e37bbb681be774",
+    "url": "/mypage/static/js/2.e0bd1538.chunk.js"
+  },
+  {
+    "revision": "35f64ee4a9be9bad1f494d8d03a071f3",
+    "url": "/mypage/static/js/2.e0bd1538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7377b98dc63450c2ca7f",
+    "url": "/mypage/static/js/main.09e65511.chunk.js"
+  },
+  {
+    "revision": "0b3f58e1508770550220",
+    "url": "/mypage/static/js/runtime-main.12f903f2.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/mypage/static/media/default-skin.b257fa9c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mypage/precache-manifest.89ca1915c5fa7c827cf3d55312d349c4.js"
+  "/mypage/precache-manifest.762a969baf1f849461744126bff5289c.js"
 );
 
 self.addEventListener('message', (event) => {

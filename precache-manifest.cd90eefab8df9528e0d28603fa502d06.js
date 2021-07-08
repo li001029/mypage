0099d74da93e9c2fd46c884f615e44dc4d9094f8
Jiafeng Li <li001029@umn.edu>
2021-07-07 22:38:38 -0500

    Updates

---
+++ precache-manifest.cd90eefab8df9528e0d28603fa502d06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9847fc73f0cf36fca0b07f84a570e71",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "b9b485e37bbb681be774",
+    "url": "/mypage/static/css/2.2c431122.chunk.css"
+  },
+  {
+    "revision": "c3d209245600340e6a40",
+    "url": "/mypage/static/css/main.5933d936.chunk.css"
+  },
+  {
+    "revision": "b9b485e37bbb681be774",
+    "url": "/mypage/static/js/2.e0bd1538.chunk.js"
+  },
+  {
+    "revision": "35f64ee4a9be9bad1f494d8d03a071f3",
+    "url": "/mypage/static/js/2.e0bd1538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d209245600340e6a40",
+    "url": "/mypage/static/js/main.3f932ca6.chunk.js"
+  },
+  {
+    "revision": "0b3f58e1508770550220",
+    "url": "/mypage/static/js/runtime-main.12f903f2.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/mypage/static/media/default-skin.b257fa9c.svg"
+  }
+]);
